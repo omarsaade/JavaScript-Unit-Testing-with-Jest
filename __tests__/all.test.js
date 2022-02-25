d49e73b3d07b525b2319495443cb4a62a8a4baaa
omar saade <omarsaadeh11@gmail.com>
2022-02-26 00:06:10 +0200

    create ur own matchers

--- __tests__/all.test.js
+++ __tests__/all.test.js
@@ -169,3 +169,31 @@ test("check if year is between year Range", () => {
 })
 
 //////////////////////////////////////////////////////////////
+
+//expect.anything() => Anything But Undefined + Null
+
+test("Expect Anything", () => {
+    let x = "A"
+        ;
+    expect("Osama").toEqual(expect.anything());
+    expect(1).toEqual(expect.anything());
+    expect([1, 2, 3, 4]).toEqual(expect.anything());
+    expect(x).toEqual(expect.anything());
+})
+
+/////////////////
+//expect.any(Constructor)
+
+test("Expect Any Constructor", () => {
+
+    expect("Osama").toEqual(expect.any(String));
+
+})
+
+//expect.arrayContaining(array)
+
+test("Expect Array To Be Found In The Main Array", () => {
+    const myArray = [1, 2, 3, 4, 5, 6]
+    expect(myArray).toEqual(expect.arrayContaining([5, 3, 4, 6]));
+
+})
